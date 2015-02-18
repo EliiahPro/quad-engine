5e0a2fa6a1011749ac7794826e867bc61d8c0a7a
Darthman <Darthman@localhost>
2015-02-18 13:28:06 +0300

    Added file-recognition by header

--- QuadEngine.TextureLoader.pas
+++ QuadEngine.TextureLoader.pas
@@ -3,61 +3,72 @@ unit QuadEngine.TextureLoader;
 interface
 
 uses
-  direct3d9, QuadEngine;
+  direct3d9, QuadEngine, Generics.Collections, sysutils;
 
 type
   TTextureSignature = array[0..31] of AnsiChar;
 
   TQuadCustomTextureFormat = class abstract
-    procedure LoadFromFile(const aFilename: String;
+    class procedure LoadFromFile(const aFilename: String;
       var Texture: IDirect3DTexture9; ColorKey: Integer); virtual; abstract;
-    function CheckSignature(ASignature: TTextureSignature): Boolean; virtual; abstract;
+    class function CheckSignature(ASignature: TTextureSignature): Boolean; virtual; abstract;
   end;
 
   TQuadBMPTextureFormat = class sealed(TQuadCustomTextureFormat)
-    procedure LoadFromFile(const aFilename: string;
+    class procedure LoadFromFile(const aFilename: string;
       var Texture: IDirect3DTexture9; ColorKey: Integer); override;
-    function CheckSignature(ASignature: TTextureSignature): Boolean; override;
+    class function CheckSignature(ASignature: TTextureSignature): Boolean; override;
   end;
 
   TQuadPNGTextureFormat = class sealed(TQuadCustomTextureFormat)
-    procedure LoadFromFile(const aFilename: string;
+    class procedure LoadFromFile(const aFilename: string;
       var Texture: IDirect3DTexture9; ColorKey: Integer); override;
-    function CheckSignature(ASignature: TTextureSignature): Boolean; override;
+    class function CheckSignature(ASignature: TTextureSignature): Boolean; override;
   end;
 
   TQuadTGATextureFormat = class sealed(TQuadCustomTextureFormat)
-    procedure LoadFromFile(const aFilename: string;
+    class procedure LoadFromFile(const aFilename: string;
       var Texture: IDirect3DTexture9; ColorKey: Integer); override;
-    function CheckSignature(ASignature: TTextureSignature): Boolean; override;
+    class function CheckSignature(ASignature: TTextureSignature): Boolean; override;
   end;
 
   TQuadJPGTextureFormat = class sealed(TQuadCustomTextureFormat)
-    procedure LoadFromFile(const aFilename: string;
+    class procedure LoadFromFile(const aFilename: string;
       var Texture: IDirect3DTexture9; ColorKey: Integer); override;
-    function CheckSignature(ASignature: TTextureSignature): Boolean; override;
+    class function CheckSignature(ASignature: TTextureSignature): Boolean; override;
   end;
 
   TQuadRAWTextureFormat = class sealed(TQuadCustomTextureFormat)
-    procedure LoadFromFile(const aFilename: string;
+    class procedure LoadFromFile(const aFilename: string;
       var Texture: IDirect3DTexture9; ColorKey: Integer); override;
-    function CheckSignature(ASignature: TTextureSignature): Boolean; override;
+    class function CheckSignature(ASignature: TTextureSignature): Boolean; override;
   end;
 
+  TQuadCustomTextureClass = class of TQuadCustomTextureFormat;
+
   TQuadTextureLoader = class
-    function LoadFromFile: IQuadTexture;
+  private
+    class var FFormats: TList<TQuadCustomTextureClass>;
+  public
+    class procedure Register(AQuadCustomTextureClass: TQuadCustomTextureClass);
+    class function LoadFromFile(AFileName: string): IQuadTexture;
   end;
+
+  TQ = (TQuadBMPTextureClass, TQuadPNGTextureClass, TQuadTGATextureClass, TQuadJPGTextureClass, TQuadRAWTextureClass);
 
 implementation
 
+uses
+  QuadEngine.Device;
+
 { TQuadJPGTextureFormat }
 
-function TQuadJPGTextureFormat.CheckSignature(ASignature: TTextureSignature): Boolean;
+class function TQuadJPGTextureFormat.CheckSignature(ASignature: TTextureSignature): Boolean;
 begin
   Result := (ASignature[6] = 'J') and (ASignature[7] = 'F') and (ASignature[8] = 'I') and (ASignature[9] = 'F');
 end;
 
-procedure TQuadJPGTextureFormat.LoadFromFile(const aFilename: string;
+class procedure TQuadJPGTextureFormat.LoadFromFile(const aFilename: string;
   var Texture: IDirect3DTexture9; ColorKey: Integer);
 begin
 
@@ -65,12 +76,12 @@ end;
 
 { TQuadRAWTextureFormat }
 
-function TQuadRAWTextureFormat.CheckSignature(ASignature: TTextureSignature): Boolean;
+class function TQuadRAWTextureFormat.CheckSignature(ASignature: TTextureSignature): Boolean;
 begin
   Result := True;
 end;
 
-procedure TQuadRAWTextureFormat.LoadFromFile(const aFilename: string;
+class procedure TQuadRAWTextureFormat.LoadFromFile(const aFilename: string;
   var Texture: IDirect3DTexture9; ColorKey: Integer);
 begin
 
@@ -78,12 +89,12 @@ end;
 
 { TQuadTGATextureFormat }
 
-function TQuadTGATextureFormat.CheckSignature(ASignature: TTextureSignature): Boolean;
+class function TQuadTGATextureFormat.CheckSignature(ASignature: TTextureSignature): Boolean;
 begin
   Result := Copy(ASignature, 1, 16) = 'TRUEVISION-XFILE';
 end;
 
-procedure TQuadTGATextureFormat.LoadFromFile(const aFilename: string;
+class procedure TQuadTGATextureFormat.LoadFromFile(const aFilename: string;
   var Texture: IDirect3DTexture9; ColorKey: Integer);
 begin
 
@@ -91,12 +102,12 @@ end;
 
 { TQuadPNGTextureFormat }
 
-function TQuadPNGTextureFormat.CheckSignature(ASignature: TTextureSignature): Boolean;
+class function TQuadPNGTextureFormat.CheckSignature(ASignature: TTextureSignature): Boolean;
 begin
   Result := Copy(ASignature, 2, 3) = 'PNG';
 end;
 
-procedure TQuadPNGTextureFormat.LoadFromFile(const aFilename: string;
+class procedure TQuadPNGTextureFormat.LoadFromFile(const aFilename: string;
   var Texture: IDirect3DTexture9; ColorKey: Integer);
 begin
 
@@ -104,15 +115,57 @@ end;
 
 { TQuadBMPTextureFormat }
 
-function TQuadBMPTextureFormat.CheckSignature(ASignature: TTextureSignature): Boolean;
+class function TQuadBMPTextureFormat.CheckSignature(ASignature: TTextureSignature): Boolean;
 begin
   Result := Copy(ASignature, 1, 2) = 'BM';
 end;
 
-procedure TQuadBMPTextureFormat.LoadFromFile(const aFilename: string;
+class procedure TQuadBMPTextureFormat.LoadFromFile(const aFilename: string;
   var Texture: IDirect3DTexture9; ColorKey: Integer);
 begin
 
 end;
+
+{ TQuadTextureLoader }
+
+class function TQuadTextureLoader.LoadFromFile(AFileName: string): IQuadTexture;
+var
+  tf: TQuadCustomTextureClass;
+  Signature: TTextureSignature;
+  res: Boolean;
+  f: file;
+begin
+  AssignFile(f, AFilename);
+  Reset(f, 1);
+  BlockRead(f, Signature[0], 31);
+  CloseFile(f);
+
+  for tf in FFormats do
+  begin
+    res := tf.CheckSignature(Signature);
+
+    if res then
+      Break;
+  end;
+
+  if not res then
+    Exit;
+end;
+
+class procedure TQuadTextureLoader.Register(AQuadCustomTextureClass: TQuadCustomTextureClass);
+begin
+  FFormats.Add(AQuadCustomTextureClass);
+end;
+
+initialization
+  TQuadTextureLoader.FFormats := TList<TQuadCustomTextureClass>.Create;
+  TQuadTextureLoader.Register(TQuadBMPTextureFormat);
+  TQuadTextureLoader.Register(TQuadPNGTextureFormat);
+  TQuadTextureLoader.Register(TQuadTGATextureFormat);
+  TQuadTextureLoader.Register(TQuadJPGTextureFormat);
+  TQuadTextureLoader.Register(TQuadRAWTextureFormat);
+
+finalization
+  TQuadTextureLoader.FFormats.Free;
 
 end.
