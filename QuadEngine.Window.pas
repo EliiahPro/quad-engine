a5104caa7c41d78544cd34130e42302a04f866ee
Darthman <Darthman@localhost>
2015-09-30 19:46:52 +0300

    Borderless mode for single metrics added

--- QuadEngine.Window.pas
+++ QuadEngine.Window.pas
@@ -474,7 +474,7 @@ begin
   Diff.X := 0;
   Diff.Y := 0;
 
-  if (AHeight >= GetSystemMetrics(SM_CYSCREEN)) and
+  if (AHeight >= GetSystemMetrics(SM_CYSCREEN)) or
      (AWidth >= GetSystemMetrics(SM_CXSCREEN)) then
     SetWindowLong(FHandle, GWL_STYLE, GetWindowLong(FHandle, GWL_STYLE) and not WS_BORDER and not WS_SIZEBOX and not WS_DLGFRAME)
   else
