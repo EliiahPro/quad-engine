986c01ebad6687e5f4d5093e1f61efbbe271cccb
ZblCoder <ZblCoder@localhost>
2016-01-29 14:54:36 +0300

    Diagram component: merge objects

--- components/Quad.Diagram.pas
+++ components/Quad.Diagram.pas
@@ -14,28 +14,21 @@ type
 
   TQuadPointChangeEvent = procedure(ADiagram: TQuadDiagram; ALine: TQuadDiagramLineItem; APoint: TQuadDiagramLinePointItem) of object;
 
-  TQuadPersistentPoint = class(TPersistent)
-  private
-    FX: Double;
-    FY: Double;
-    procedure SetX(const Value: Double);
-    procedure SetY(const Value: Double);
-  published
-    property X: Double read FX write SetX;
-    property Y: Double read FY write SetY;
-  end;
   //
   TQuadDiagramLinePointItem = class(TCollectionItem)
   private
-    FPoint: TQuadPersistentPoint;
+    FX: Double;
+    FY: Double;
     FColor: TColor;
-    procedure SetPoint(const Value: TQuadPersistentPoint);
+    procedure SetX(const Value: Double);
+    procedure SetY(const Value: Double);
     procedure SetColor(const Value: TColor);
   public
     constructor Create(Collection: TCollection); override;
     destructor Destroy; override;
   published
-    property Point: TQuadPersistentPoint read FPoint write SetPoint;
+    property X: Double read FX write SetX;
+    property Y: Double read FY write SetY;
     property Color: TColor read FColor write SetColor;
   end;
   //
@@ -172,9 +165,9 @@ type
     FLineMouseMoveIndex: Integer;
     FPointMouseMoveIndex: Integer;
     FMouse: TGPPointF;
-    FPointSelected: TQuadPersistentPoint;
-    FMaxNotPointSelected: TQuadPersistentPoint;
-    FMinNotPointSelected: TQuadPersistentPoint;
+    FPointSelected: TQuadDiagramLinePointItem;
+    FMaxNotPointSelected: TQuadDiagramLinePointItem;
+    FMinNotPointSelected: TQuadDiagramLinePointItem;
     FPosition: Double;
 
     FAxisV: TQuadDiagramAxis;
@@ -199,8 +192,8 @@ type
     procedure DrawPoint(APoint: TQuadDiagramLinePointItem);
     procedure SetStyle(const Value: TQuadDiagramStyle);
     procedure SetLines(const Value: TQuadDiagramLineCollection);
-    function PointToGridPoint(APoint: TQuadPersistentPoint): TGPPointF;
-    procedure GridPointToPoint(APoint: TQuadPersistentPoint; X, Y: Double);
+    function PointToGridPoint(APoint: TQuadDiagramLinePointItem): TGPPointF;
+    procedure GridPointToPoint(APoint: TQuadDiagramLinePointItem; X, Y: Double);
     function GetGPRect: TGPRect;
     function PointMouseMove(const Point: TGPPointF): Boolean;
     function LineMouseMove(const Point1, Point2: TGPPointF): Boolean;
@@ -211,8 +204,8 @@ type
     function GetValueInPosition(ALine: TQuadDiagramLineItem; APosition: Double): Double;
     procedure SetAxisV(const Value: TQuadDiagramAxis);
     procedure SetAxisH(const Value: TQuadDiagramAxis);
-    procedure SetPointSelected(const Value: TQuadPersistentPoint);
-    property PointSelected: TQuadPersistentPoint read FPointSelected write SetPointSelected;
+    procedure SetPointSelected(const Value: TQuadDiagramLinePointItem);
+    property PointSelected: TQuadDiagramLinePointItem read FPointSelected write SetPointSelected;
   protected
     procedure Paint; override;
     procedure MouseMove(Shift: TShiftState; X: Integer; Y: Integer); override;
@@ -380,13 +373,13 @@ begin
   Result := sqr(Point.X - FMouse.X) + sqr(Point.Y - FMouse.Y) < 16;
 end;
 
-function TQuadDiagram.PointToGridPoint(APoint: TQuadPersistentPoint): TGPPointF;
+function TQuadDiagram.PointToGridPoint(APoint: TQuadDiagramLinePointItem): TGPPointF;
 begin
   Result.X := FPaintWidth / (FAxisH.MaxValue - FAxisH.MinValue) * (APoint.X - FAxisH.MinValue) + FPaintRect.Left;
   Result.Y := FPaintRect.Bottom - FPaintHeight / (FAxisV.MaxValue - FAxisV.MinValue) * (APoint.Y - FAxisV.MinValue);
 end;
 
-procedure TQuadDiagram.GridPointToPoint(APoint: TQuadPersistentPoint; X, Y: Double);
+procedure TQuadDiagram.GridPointToPoint(APoint: TQuadDiagramLinePointItem; X, Y: Double);
 begin
   APoint.X := ((FAxisH.MaxValue - FAxisH.FMinValue) / FPaintWidth * (X - FPaintRect.Left)) + FAxisH.MinValue;
   APoint.Y := ((FAxisV.MaxValue - FAxisV.FMinValue) / FPaintHeight * (FPaintHeight - Y + FPaintRect.Top)) + FAxisV.MinValue;
@@ -638,11 +631,11 @@ begin
 
   FPen := TGPPen.Create(ColorToARGB(ALine.Color), ALine.Width);
   FPen.SetDashStyle(ALine.Style);
-  PointL := PointToGridPoint(ALine.Points.Items[0].Point);
+  PointL := PointToGridPoint(ALine.Points.Items[0]);
   DrawSegment(FPen, FPaintRect.Left, PointL.Y, PointL.X, PointL.Y);
   for i := 0 to ALine.Points.Count - 1 do
   begin
-    PointR := PointToGridPoint(ALine.Points.Items[i].Point);
+    PointR := PointToGridPoint(ALine.Points.Items[i]);
     DrawSegment(FPen, PointL.X, PointL.Y, PointR.X, PointR.Y);
     PointL := PointR;
   end;
@@ -675,7 +668,7 @@ procedure TQuadDiagram.DrawPoint(APoint: TQuadDiagramLinePointItem);
 var
   Point: TGPPointF;
 begin
-  Point := PointToGridPoint(APoint.Point);
+  Point := PointToGridPoint(APoint);
   FGraphics.FillEllipse(FBrush, Point.X - 3, Point.Y - 3, 6, 6);
   FGraphics.DrawEllipse(FPen, Point.X - 3, Point.Y - 3, 6, 6);
      {
@@ -704,7 +697,7 @@ begin
   Result := GetValueInPosition(FLines.Items[ALine], FPosition);
 end;
 
-procedure TQuadDiagram.SetPointSelected(const Value: TQuadPersistentPoint);
+procedure TQuadDiagram.SetPointSelected(const Value: TQuadDiagramLinePointItem);
 var
   i: Integer;
   Points: TQuadDiagramLinePointCollection;
@@ -715,13 +708,13 @@ begin
   begin
     Points := FLines.Items[FLineMouseMoveIndex].Points;
     for i := 0 to Points.Count - 1 do
-      if Points.Items[i].Point <> Value then
+      if Points.Items[i] <> Value then
       begin
-        if (FMaxNotPointSelected = nil) or (Points.Items[i].Point.Y > FMaxNotPointSelected.Y) then
-          FMaxNotPointSelected := Points.Items[i].Point;
+        if (FMaxNotPointSelected = nil) or (Points.Items[i].Y > FMaxNotPointSelected.Y) then
+          FMaxNotPointSelected := Points.Items[i];
 
-        if (FMinNotPointSelected = nil) or (Points.Items[i].Point.Y < FMinNotPointSelected.Y) then
-          FMinNotPointSelected := Points.Items[i].Point;
+        if (FMinNotPointSelected = nil) or (Points.Items[i].Y < FMinNotPointSelected.Y) then
+          FMinNotPointSelected := Points.Items[i];
       end;
   end;
   FPointSelected := Value;
@@ -732,19 +725,19 @@ var
   i: Integer;
 begin
   for i := 0 to ALine.Points.Count - 1 do
-    if (i = 0) and (ALine.Points.Items[i].Point.X >= APosition) or
-       (i = ALine.Points.Count - 1) and (ALine.Points.Items[i].Point.X <= APosition) then
+    if (i = 0) and (ALine.Points.Items[i].X >= APosition) or
+       (i = ALine.Points.Count - 1) and (ALine.Points.Items[i].X <= APosition) then
     begin
-      Result := ALine.Points.Items[i].Point.Y;
+      Result := ALine.Points.Items[i].Y;
       Exit;
     end
     else
-      if (i <> 0) and (ALine.Points.Items[i-1].Point.X <= APosition) and
-         (APosition <= ALine.Points.Items[i].Point.X) then
+      if (i <> 0) and (ALine.Points.Items[i-1].X <= APosition) and
+         (APosition <= ALine.Points.Items[i].X) then
       begin
-        Result := ALine.Points.Items[i-1].Point.Y + (ALine.Points.Items[i].Point.Y - ALine.Points.Items[i-1].Point.Y) /
-                  (ALine.Points.Items[i].Point.X - ALine.Points.Items[i-1].Point.X) *
-                  (APosition - ALine.Points.Items[i-1].Point.X);
+        Result := ALine.Points.Items[i-1].Y + (ALine.Points.Items[i].Y - ALine.Points.Items[i-1].Y) /
+                  (ALine.Points.Items[i].X - ALine.Points.Items[i-1].X) *
+                  (APosition - ALine.Points.Items[i-1].X);
         Exit;
       end;
   Result := 0;
@@ -756,7 +749,7 @@ begin
   begin
     if FPointMouseMove then
     begin
-      PointSelected := FLines.Items[FLineMouseMoveIndex].Points.Items[FPointMouseMoveIndex].Point;
+      PointSelected := FLines.Items[FLineMouseMoveIndex].Points.Items[FPointMouseMoveIndex];
       FLineMouseMove := False;
       Repaint;
     end
@@ -765,9 +758,9 @@ begin
       begin
         FPointMouseMove := False;
         if FPointMouseMoveIndex > -1 then
-          PointSelected := FLines.Items[FLineMouseMoveIndex].Points.Insert(FPointMouseMoveIndex).Point
+          PointSelected := FLines.Items[FLineMouseMoveIndex].Points.Insert(FPointMouseMoveIndex)
         else
-          PointSelected := FLines.Items[FLineMouseMoveIndex].Points.Add.Point;
+          PointSelected := FLines.Items[FLineMouseMoveIndex].Points.Add;
         GridPointToPoint(PointSelected, FMouse.X, FMouse.Y);
         if Assigned(FOnPointAdd) then
           FOnPointAdd(
@@ -822,11 +815,11 @@ begin
   begin
     GridPointToPoint(PointSelected, FMouse.X, FMouse.Y);
 
-    if (FPointMouseMoveIndex > 0) and (FLines.Items[FLineMouseMoveIndex].Points.Items[FPointMouseMoveIndex-1].Point.X > PointSelected.X) then
-      PointSelected.X := FLines.Items[FLineMouseMoveIndex].Points.Items[FPointMouseMoveIndex-1].Point.X
+    if (FPointMouseMoveIndex > 0) and (FLines.Items[FLineMouseMoveIndex].Points.Items[FPointMouseMoveIndex-1].X > PointSelected.X) then
+      PointSelected.X := FLines.Items[FLineMouseMoveIndex].Points.Items[FPointMouseMoveIndex-1].X
     else
-      if (FPointMouseMoveIndex < FLines.Items[FLineMouseMoveIndex].Points.Count - 1) and (FLines.Items[FLineMouseMoveIndex].Points.Items[FPointMouseMoveIndex+1].Point.X < PointSelected.X) then
-        PointSelected.X := FLines.Items[FLineMouseMoveIndex].Points.Items[FPointMouseMoveIndex+1].Point.X
+      if (FPointMouseMoveIndex < FLines.Items[FLineMouseMoveIndex].Points.Count - 1) and (FLines.Items[FLineMouseMoveIndex].Points.Items[FPointMouseMoveIndex+1].X < PointSelected.X) then
+        PointSelected.X := FLines.Items[FLineMouseMoveIndex].Points.Items[FPointMouseMoveIndex+1].X
       else
         if PointSelected.X < FAxisH.MinValue then
           PointSelected.X := FAxisH.MinValue
@@ -891,7 +884,7 @@ begin
         for p := 0 to FLines.Items[i].Points.Count - 1 do
         begin
           FPointMouseMoveIndex := p;
-          if PointMouseMove(PointToGridPoint(FLines.Items[i].Points.Items[p].Point)) then
+          if PointMouseMove(PointToGridPoint(FLines.Items[i].Points.Items[p])) then
           begin
             SetPointMouseMove(True);
             Exit;
@@ -906,12 +899,12 @@ begin
       if FLines.Items[i].Enabled then
       begin
         FLineMouseMoveIndex := i;
-        PointL := PointToGridPoint(FLines.Items[i].Points.Items[0].Point);
+        PointL := PointToGridPoint(FLines.Items[i].Points.Items[0]);
         PointL.X := FPaintRect.Left;
         for p := 0 to FLines.Items[i].Points.Count - 1 do
         begin
           FPointMouseMoveIndex := p;
-          PointR := PointToGridPoint(FLines.Items[i].Points.Items[p].Point);
+          PointR := PointToGridPoint(FLines.Items[i].Points.Items[p]);
           if LineMouseMove(PointL, PointR) then
           begin
             SetLineMouseMove(True);
@@ -1092,12 +1085,13 @@ end;
 constructor TQuadDiagramLinePointItem.Create(Collection: TCollection);
 begin
   inherited Create(Collection);
-  FPoint := TQuadPersistentPoint.Create;
+  FColor := 0;
+  FX := 0;
+  FY := 0;
 end;
 
 destructor TQuadDiagramLinePointItem.Destroy;
 begin
-  FPoint.Free;
   inherited;
 end;
 
@@ -1106,9 +1100,14 @@ begin
   FColor := Value;
 end;
 
-procedure TQuadDiagramLinePointItem.SetPoint(const Value: TQuadPersistentPoint);
+procedure TQuadDiagramLinePointItem.SetX(const Value: Double);
 begin
-  FPoint.Assign(Value);
+  FX := Value;
+end;
+
+procedure TQuadDiagramLinePointItem.SetY(const Value: Double);
+begin
+  FY := Value;
 end;
 
 { TQuadDiagramLinePointCollection }
@@ -1131,8 +1130,8 @@ end;
 function TQuadDiagramLinePointCollection.Add(X, Y: Double): TQuadDiagramLinePointItem;
 begin
   Result := TQuadDiagramLinePointItem(inherited Add);
-  Result.Point.FX := X;
-  Result.Point.FY := Y;
+  Result.FX := X;
+  Result.FY := Y;
 end;
 
 function TQuadDiagramLinePointCollection.GetItem(Index: Integer): TQuadDiagramLinePointItem;
@@ -1148,18 +1147,6 @@ end;
 procedure TQuadDiagramLinePointCollection.SetItem(Index: Integer; const Value: TQuadDiagramLinePointItem);
 begin
   Items[index].Assign(Value);
-end;
-
-{ TQuadPersistentPoint }
-
-procedure TQuadPersistentPoint.SetX(const Value: Double);
-begin
-  FX := Value;
-end;
-
-procedure TQuadPersistentPoint.SetY(const Value: Double);
-begin
-  FY := Value;
 end;
 
 { TQuadDiagramAxis }
@@ -1245,149 +1232,5 @@ procedure TQuadDiagramAxis.SetHighMax(Value: Single);
 begin
   FHighMax := Value;
 end;
-             {
-constructor TQuadDiagram.Create(AOwner: TComponent);
-begin
-  inherited Create(AOwner);
 
-  FAxisV.Name := 'Angle ''';
-  FAxisV.MaxValue := 360;
-  FAxisV.WidthValue := 90;
-  FAxisV.Format := '0';
-
-  FAxisH.Name := 'Life %';
-  FAxisH.MaxValue := 100;
-  FAxisH.WidthValue := 20;
-  FAxisH.Format := '0';
-
-  FLineCount := 1;
-  SetLength(FLines, FLineCount);
-  FLines[0].PointCount := 4;
-  SetLength(FLines[0].Points, FLines[0].PointCount);
-  FLines[0].Points[0] := MakePoint(0, 90.0);
-  FLines[0].Points[1] := MakePoint(10, 180.0);
-  FLines[0].Points[2] := MakePoint(60, 270.0);
-  FLines[0].Points[3] := MakePoint(100, 180.0);
-end;
-
-destructor TQuadDiagram.Destroy;
-begin
-
-  inherited;
-end;
-
-procedure TQuadDiagram.Paint;
-var
-  i, k: Integer;
-  W: TGPPointF;
-  Points: TPointFDynArray;
-begin
-  FGraphics := TGPGraphics.Create(Canvas.Handle);
-  FGraphics.SetSmoothingMode(SmoothingModeAntiAlias);
-  FPen := TGPPen.Create(ColorToARGB(stSystem, clAppWorkSpace));
-  FFontFamily := TGPFontFamily.Create('Tahoma');
-  FFontFormat := TGPStringFormat.Create;
-  FFont := TGPFont.Create(FFontFamily, 11, FontStyleRegular, UnitPixel);
-  FBrush := TGPSolidBrush.Create(ColorToARGB(stSystem, clBtnFace));
-
-  FRect.Right := Width - 20;
-  FRect.Top := 20;
-  FRect.Bottom := Height - 30;
-  FRect.Left := 30;
-
-  FGraphics.FillRectangle(FBrush, MakeRect(0, 0, Width, Height));
-
-  FPen.SetColor(ColorToARGB(stSystem, clBtnHighlight));
-  FGraphics.DrawLine(FPen, Width-1, 0, Width-1, Height);
-  FGraphics.DrawLine(FPen, 0, Height-1, Width, Height-1);
-
-  FPen.SetColor(ColorToARGB(stSystem, clBtnShadow));
-  FGraphics.DrawLine(FPen, 0, 0, Width, 0);
-  FGraphics.DrawLine(FPen, 0, 0, 0, Height);
-
-  DrawGrid;
-
-  W := MakePoint(
-    (FRect.Right - FRect.Left) / FAxisH.MaxValue,
-    (FRect.Bottom - FRect.Top) / FAxisV.MaxValue
-  );
-  FPen.SetWidth(2);
-  FPen.SetColor(ColorToARGB(stNone, TStyleManager.ActiveStyle.GetStyleFontColor(sfTextLabelNormal)));
-  for i := 0 to FLineCount - 1 do
-  begin
-    SetLength(Points, FLines[i].PointCount);
-    for k := 0 to FLines[i].PointCount - 1 do
-      Points[k] := MakePoint(
-        FRect.Left + W.X * FLines[i].Points[k].X,
-        FRect.Bottom - W.Y * FLines[i].Points[k].Y
-      );
-    for k := 1 to FLines[i].PointCount - 1 do
-      FGraphics.DrawLine(FPen, Points[k - 1], Points[k])
-   // FGraphics.DrawLines(FPen, PGPPoint(Points), FLines[i].PointCount);
-  end;
-
-  FFontFormat.Free;
-  FPen.Free;
-  FGraphics.Free;
-end;
-
-
-procedure TQuadDiagram.DrawGrid;
-var
-  i: Integer;
-  F, W: Double;
-  Str: String;
-  Points: TPointFDynArray;
-begin
-  FPen.SetWidth(1);
-  FPen.SetColor(ColorToARGB(stNone, TStyleManager.ActiveStyle.GetStyleFontColor(sfTextLabelNormal)));
-  FBrush.SetColor(ColorToARGB(stNone, TStyleManager.ActiveStyle.GetStyleFontColor(sfTextLabelNormal)));
-
-  FGraphics.DrawString(FAxisV.Name, Length(FAxisV.Name), FFont, MakePoint(FRect.Left + 8.0, FRect.Top - 18.0), FBrush);
-  FFontFormat.SetAlignment(StringAlignmentFar);
-  FGraphics.DrawString(FAxisH.Name, Length(FAxisH.Name), FFont, MakePoint(FRect.Right + 16, FRect.Bottom + 14.0), FFontFormat, FBrush);
-
-  FFontFormat.SetAlignment(StringAlignmentFar);
-  FGraphics.DrawString('0', 1, FFont, MakePoint(FRect.Left, FRect.Bottom + 2.0), FFontFormat, FBrush);
-
-  FPen.SetColor(ColorToARGB(stSystem, clAppWorkSpace));
-  FPen.SetDashStyle(DashStyleDash);
-  FFontFormat.SetAlignment(StringAlignmentCenter);
-  W := (FRect.Right - FRect.Left) / FAxisH.MaxValue * FAxisH.WidthValue;
-  for i := 1 to Round(FAxisH.MaxValue / FAxisH.WidthValue) do
-  begin
-    F := Round(FRect.Left + W * i);
-    Str := FormatFloat(FAxisH.Format, FAxisH.WidthValue * i);
-    FGraphics.DrawLine(FPen, F, FRect.Top, F, FRect.Bottom);
-    FGraphics.DrawString(Str, Length(Str), FFont, MakePoint(F, FRect.Bottom + 2), FFontFormat, FBrush);
-  end;
-
-  FFontFormat.SetAlignment(StringAlignmentFar);
-  W := (FRect.Bottom - FRect.Top) / FAxisV.MaxValue * FAxisV.WidthValue;
-  for i := 1 to Round(FAxisV.MaxValue / FAxisV.WidthValue) do
-  begin
-    F := Round(FRect.Bottom - W * i);
-    Str := FormatFloat(FAxisV.Format, FAxisV.WidthValue * i);
-    FGraphics.DrawLine(FPen, FRect.Left, F, FRect.Right, F);
-    FGraphics.DrawString(Str, Length(Str), FFont, MakePoint(FRect.Left - 2, F - 7), FFontFormat, FBrush);
-  end;
-
-  FPen.SetDashStyle(DashStyleSolid);
-  FPen.SetColor(ColorToARGB(stNone, TStyleManager.ActiveStyle.GetStyleFontColor(sfTextLabelNormal)));
-
-  FGraphics.DrawLine(FPen, FRect.Left, FRect.Top - 10, FRect.Left, FRect.Bottom);
-  FGraphics.DrawLine(FPen, FRect.Left, FRect.Bottom, FRect.Right + 10, FRect.Bottom);
-  SetLength(Points, 4);
-  Points[0] := MakePoint(FRect.Right + 4, FRect.Bottom - 3.5);
-  Points[1] := MakePoint(FRect.Right + 7, FRect.Bottom + 0.0);
-  Points[2] := MakePoint(FRect.Right + 4, FRect.Bottom + 3.5);
-  Points[3] := MakePoint(FRect.Right + 16, FRect.Bottom + 0.0);
-  FGraphics.FillPolygon(FBrush, PGPPointF(@Points[0]), 4);
-  Points[0] := MakePoint(FRect.Left + 3.5, FRect.Top - 4);
-  Points[1] := MakePoint(FRect.Left + 0.0, FRect.Top - 7);
-  Points[2] := MakePoint(FRect.Left - 3.5, FRect.Top - 4);
-  Points[3] := MakePoint(FRect.Left + 0.0, FRect.Top - 14);
-  FGraphics.FillPolygon(FBrush, PGPPointF(@Points[0]), 4);
-end;
-  }
 end.
