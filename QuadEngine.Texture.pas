f088dadffd6fb44d6e6d92700475ccd6457246dc
Stanislav <stas@forji.technology>
2017-01-04 23:51:42 +0300

    Restored missing TQuadTexture.DrawPartRotScaledAxis.

--- QuadEngine.Texture.pas
+++ QuadEngine.Texture.pas
@@ -20,7 +20,7 @@ uses
   QuadEngine, System.SyncObjs, Vec2f, QuadEngine.TextureLoader;
 
 type
-  TQuadTexture =  class(TInterfacedObject, IQuadTexture)
+  TQuadTexture = class(TInterfacedObject, IQuadTexture)
   private
     FQuadRender: TQuadRender;
     FTextures: array of IDirect3DTexture9;
@@ -58,6 +58,7 @@ type
     procedure DrawPart(const Position: TVec2f; LeftTop, RightBottom: TVec2i; Color: Cardinal = $FFFFFFFF); stdcall;
     procedure DrawPartRot(const Center: TVec2f; angle, Scale: Double; LeftTop, RightBottom: TVec2i; Color: Cardinal = $FFFFFFFF); stdcall;
     procedure DrawPartRotAxis(const Position: TVec2f; angle, Scale: Double; const Axis: TVec2f; LeftTop, RightBottom: TVec2i; Color: Cardinal = $FFFFFFFF); stdcall;
+    procedure DrawPartRotScaledAxis(const Position: TVec2f; angle: Double; const Scale, Axis: TVec2f; LeftTop, RightBottom: TVec2i; Color: Cardinal = $FFFFFFFF); stdcall;
     procedure DrawRot(const Center: TVec2f; angle, Scale: Double; Color: Cardinal = $FFFFFFFF); stdcall;
     procedure DrawRotFrame(const Center: TVec2f; angle, Scale: Double; Pattern: Word; Color: Cardinal = $FFFFFFFF); stdcall;
     procedure DrawRotAxis(const Position: TVec2f; angle, Scale: Double; const Axis: TVec2f; Color: Cardinal = $FFFFFFFF); stdcall;
@@ -244,6 +245,22 @@ begin
   SetTextureStages;
 
   FQuadRender.DrawRectRotAxis(Position - 0.5, Position - 0.5 + RightBottom - LeftTop, angle, Scale, Axis,
+                              TVec2f.Create(LeftTop.X / FWidth, LeftTop.Y / FHeight) , TVec2f.Create(RightBottom.X / FWidth, RightBottom.Y / FHeight),
+                              Color);
+end;
+
+//=============================================================================
+//
+//=============================================================================
+procedure TQuadTexture.DrawPartRotScaledAxis(const Position: TVec2f; angle: Double; const Scale, Axis: TVec2f; LeftTop, RightBottom: TVec2i;
+  Color: Cardinal);
+begin
+  if not FIsLoaded then
+    Exit;
+
+  SetTextureStages;
+
+  FQuadRender.DrawRectRotAxis(Position - 0.5, Position - 0.5 + (RightBottom - LeftTop) * Scale, angle, 1, Axis,
                               TVec2f.Create(LeftTop.X / FWidth, LeftTop.Y / FHeight) , TVec2f.Create(RightBottom.X / FWidth, RightBottom.Y / FHeight),
                               Color);
 end;
